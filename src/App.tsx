92b9a6aed14432d38e767c04cb74d5f86325f3f8
Adam Ho <adam.ho.goldenowl@gmail.com>
2023-10-08 18:38:13 +0700

    done day1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import './style.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card__header">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="card__icon"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                    </svg>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="card__icon"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                        />
+                    </svg>
+                </div>
+                <div className="card__content">
+                    <img
+                        src="layout/images/Nike Zoom KD 12.png"
+                        alt="Nike Zoom KD 12"
+                    />
+
+                    <h1>Nike Zoom KD 12</h1>
+
+                    <div className="card__price">$98</div>
+                </div>
+                <div className="card__feature">
+                    <div className="card__option-size">
+                        <div>Size:</div>
+                        <div className="card__box">6</div>
+                        <div className="card__box">7</div>
+                        <div className="card__box">8</div>
+                        <div className="card__box">9</div>
+                    </div>
+                    <div className="card__option-color">
+                        <div>Color:</div>
+                        <div className="card__circle color_lime"></div>
+                        <div className="card__circle color_red"></div>
+                        <div className="card__circle color_gray"></div>
+                    </div>
+                    <div className="card__button">
+                        <div className="">Buy now</div>
+                        <div className="">Add cart</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
